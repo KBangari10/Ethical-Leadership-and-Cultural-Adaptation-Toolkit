731f269fac6af2ac7b8c651e0bb1b037becb3f01
KBangari10 <KBangari10@students.Cumberland.edu>
2025-08-03 14:23:30 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function downloadFile(filePath) {
+    const link = document.createElement('a');
+    link.href = filePath;
+    link.download = filePath.split('/').pop();
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+}
